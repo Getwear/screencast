b494678291cada6c9982f02afee555448e92aad6
Artem Polikarpov <artpolikarpov@gmail.com>
2013-11-04 10:50:20 +0400

    Вынес контролер скринкаста в отдельный файл

---
+++ movie/js/screncast-control.js
@@ -0,0 +1,126 @@
+$(function () {
+  var $stage = $('#screencast-stage'),
+      $fotorama = $('#screencast-fotorama'),
+      fotorama,
+      currentScreencast,
+      screencastIsPlaying,
+      screencastIsEnded,
+      lastActiveFrame,
+      $play = $('#screencast-play'),
+      $replay = $('#screencast-replay'),
+      $navLinks = $('.switch', '#screencast-nav');
+
+  $fotorama
+      .on('fotorama:showend', function (e, fotorama, extra) {
+        var activeFrame = fotorama.activeFrame,
+            $frame = activeFrame.$stageFrame;
+
+        if (!$frame.data('state')) {
+          $frame.on('f:load f:error', function () {
+            fotorama.activeFrame.$stageFrame === $frame && catchFrame(activeFrame, $frame);
+          });
+        } else {
+          catchFrame(activeFrame, $frame);
+        }
+      })
+      .on('click', function () {
+        if (currentScreencast) {
+          // Пауза
+          currentScreencast[screencastIsPlaying ? 'pause' : 'resume']();
+          screencastIsPlaying = !screencastIsPlaying;
+        }
+      });
+
+  function catchFrame (activeFrame, $frame) {
+    if (lastActiveFrame === activeFrame) {
+      return;
+    }
+
+    if (currentScreencast) {
+      // Останавливаем текущий кусочек скринкаста
+      currentScreencast.stop();
+    }
+
+    var screencast = $('.screencast', $frame)
+        .screencast()
+        .data('screencast');
+
+    if (screencast) {
+      // Запускаем новый кусочек
+      // Глобальны window.currentScreencast временно, для управления из консоли (currentScreencast.start())
+      window.currentScreencast = currentScreencast = screencast;
+      screencast.start();
+      screencastIsPlaying = true;
+    }
+
+    // Выделяем активную ссылоку под скринкастом
+    $navLinks
+        .filter('[data-section="' + activeFrame.section + '"]')
+        .addClass('switch_selected')
+        .siblings()
+        .removeClass('switch_selected');
+
+    lastActiveFrame = activeFrame;
+  }
+
+  function fotoramaTurnOn () {
+    fotorama = $fotorama
+        .stop()
+        .fadeIn('fast')
+        .fotorama({
+          width: $stage.width(),
+          height: $stage.height(),
+          nav: false,
+          transition: 'dissolve',
+          trackpad: false,
+          arrows: false,
+          click: false,
+          swipe: false,
+          fit: 'none'
+        })
+        .data('fotorama');
+  }
+
+  window.fotoramaGoTo = function fotoramaGoTo (id) {
+    if (!fotorama) {
+      // Если фоторама ещё не инициирована, включаем её
+      fotoramaTurnOn();
+    }
+
+    if (screencastIsEnded) {
+      screencastIsEnded = false;
+
+      $replay.stop().fadeOut('fast', function () {
+        $replay.hide();
+      });
+    }
+
+    fotorama.show({index: id});
+  };
+
+  $play.add($replay).on('click', function () {
+    // Заводим фотораму
+    fotoramaGoTo('home');
+  });
+
+  $navLinks.on('click', function () {
+    // Переход к началу секции
+    fotoramaGoTo($(this).data('section'));
+  });
+
+  $(document).on('complete', '.screencast', function () {
+    // Слушаем события о завершении анимации скринкаста
+    if (fotorama) {
+      if (fotorama.activeIndex < fotorama.size - 1) {
+        // Включаем следующий кадр
+        fotorama.show('>');
+      } else {
+        // Останавливаем всё
+        $replay.fadeIn('fast');
+        screencastIsPlaying = false;
+        screencastIsEnded = true;
+        $stage.removeClass('stage__played');
+      }
+    }
+  });
+});
